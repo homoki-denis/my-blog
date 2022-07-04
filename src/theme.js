5175c65ef4b728012ce33376a796b66610c1adf5
Denis Homoki <homoki.denisth@gmail.com>
2022-07-04 10:52:08 +0300

    first commit

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#784bbf",
+    },
+    background: {
+      default: "#fff",
+    },
+  },
+});
+
+export default theme;
