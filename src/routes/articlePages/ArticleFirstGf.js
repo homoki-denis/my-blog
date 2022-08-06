1cf47c21708204548c5c63a8e588a14fee6e056d
Denis Homoki <homoki.denisth@gmail.com>
2022-08-06 15:00:51 +0300

    added img to gf article

--- src/routes/articlePages/ArticleFirstGf.js
+++ src/routes/articlePages/ArticleFirstGf.js
@@ -10,7 +10,7 @@ const ArticleMyFirstGf = function () {
           <CardMedia
             component="img"
             height="400"
-            image="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
+            image="https://images.unsplash.com/photo-1591711696773-c4b7fe4d3d74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
             alt="green iguana"
             sx={{ maxWidth: 1000 }}
           />
